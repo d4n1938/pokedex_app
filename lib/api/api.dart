e68d724d74ca025ee7659def451434b9e053154a
Lnida <91710828+d4n1938@users.noreply.github.com>
2023-07-10 01:08:51 +0900

    起動時の画王設定

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 Future getAllPokemon() async {
   return http.get(Uri.parse(
-      'https://pokeapi.co/api/v2/pokemon?limit=151&offset=0/language/1/'));
+      'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0/language/1/'));
 }
 
 Future getJapaneseName(String nom) async {
